705158a28238ff2f9ec8f3206885c77d903f3fa1
Jefferson <jpecht@naco.org>
2014-08-04 17:01:08 -0400

    blind code update

--- main.js
+++ main.js
@@ -1,35 +1,85 @@
 window.onload = function() {
 	
+	// create grid
 	var height = 400,
 		width = 400,
-		step = 20;
+		step = 20;	
 	
-	for (var i = 0; i < width; i += step) {
-		for (var j = 0; j < height; j += step) {
+	for (var i = 0; i < height; i += step) {
+		for (var j = 0; j < width; j += step) {
 			// create cells
 			$('<div class="cell"></div>').appendTo('#frame')
 				.css('width', step + 'px')
 				.css('height', step + 'px')
-				.css('top', i + 'px')
-				.css('left', j + 'px');
+				.css('left', i + 'px')
+				.css('top', j + 'px');
 
 			// create dividers
 			if (i === 0 && j !== 0) {
 				$('<div class="divider"></div>').appendTo('#frame')
+					.css('width', width + 'px')
+					.css('height', '1px')
+					.css('top', j + 'px')
+					.css('left', '0px');					
+			} else if (j === 0 && i !== 0) {
+				$('<div class="divider"></div>').appendTo('#frame')
 					.css('width', '1px')
 					.css('height', height + 'px')
 					.css('top', '0px')
-					.css('left', j + 'px');
-			} else if (j === 0 && i !== 0) {
-				$('<div class="divider"></div>').appendTo('#frame')
-					.css('width', width + 'px')
-					.css('height', '1px')
-					.css('top', i + 'px')
-					.css('left', '0px');					
+					.css('left', i + 'px');
 			}
 		}
 			
 	}
+	
+	
+	$('.cell').click(function() {
+		$(this).toggleClass('active');
+	});
+	
+	
+	var state = [], future_state = [];
+	for (var i = 0; i < height/step; i++) {
+		state[i] = [];
+		future_state[i] = [];
+		for (var j = 0; j < width/step; j++) {
+			state[i][j] = false;
+			future_state[i][j] = false;
+		}
+	}
+	
+	var update = function() {
+		for (var i = 0; i < state.length; i++) {
+			for (var j = 0; j < state[i].length; j++) {
+				var cell = state[i][j];
+				var num_neighbors = 0;
+				if (i !== 0) {
+					if (j !== 0 && state[i-1][j-1]) num_neighbors++;
+					if (state[i-1][j]) num_neighbors++;
+					if (j !== state[i].length - 1 && state[i-1][j+1]) num_neighbors++;
+				}
+				if (j !== 0 && state[i][j-1]) num_neighbors++;
+				if (j !== state[i].length - 1 && state[i][j+1]) num_neighbors++;
+				if (i !== state.length - 1) {
+					if (j !== 0 && state[i+1][j-1]) num_neighbors++;
+					if (state[i+1][j]) num_neighbors++;
+					if (j !== state[i].length - 1 && state[i+1][j+1]) num_neighbors++;
+				}
+				
+				if (num_neighbors === 2 || num_neighbors === 3) {
+					future_state[i][j] = (state[i][j] === true || num_neighbors === 3);
+				} else {
+					future_state[i][j] = false;
+				}
+			}
+		}
+		state = future_state;
+		setColor();
+	};
+	
+	var setColor = function() {
+		
+	};
 	
 	
 };
